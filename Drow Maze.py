db11dcb48cc47ce95a03c8a913a06efde96087f4
Rawdabadran <rawdahamam005@gmail.com>
2025-12-19 13:49:39 +0200

    Refactor maze generation and improve readability

--- Drow Maze.py
+++ Drow Maze.py
@@ -47,16 +47,20 @@ class Backtracking:
 
         self.generator(sx, sy, maze)
 
+        maze[maze == 0.5] = 1
 
-        for i in range(self.height):
-            for j in range(self.width):
-                if maze[i, j] == 0.5:
-                    maze[i, j] = 1
+        maze[0, :] = 0
+        maze[-1, :] = 0
+        maze[:, 0] = 0
+        maze[:, -1] = 0
 
+        self.start = (1, 2)
+        self.end = (self.height - 2, self.width - 3)
+        self.end2 = (self.height - 2, 8)
 
-        maze[1, 2] = 1
-        maze[self.height - 2, self.width - 3] = 1
-        maze[self.height - 2, 8] = 1
+        maze[self.start] = 1
+        maze[self.end] = 1
+        maze[self.end2] = 1
 
 
         if self.displayMaze:
@@ -72,50 +76,28 @@ class Backtracking:
         return 0
 
     def generator(self, cx, cy, grid):
-
         grid[cy, cx] = 0.5
+        directions = [Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT]
+        random.shuffle(directions)
 
+        for d in directions:
+            if d == Directions.UP:
+                nx, ny = cx, cy - 2
+                mx, my = cx, cy - 1
+            elif d == Directions.DOWN:
+                nx, ny = cx, cy + 2
+                mx, my = cx, cy + 1
+            elif d == Directions.LEFT:
+                nx, ny = cx - 2, cy
+                mx, my = cx - 1, cy
+            else:
+                nx, ny = cx + 2, cy
+                mx, my = cx + 1, cy
 
-        if (grid[cy - 2, cx] == 0.5 and grid[cy + 2, cx] == 0.5 and
-            grid[cy, cx - 2] == 0.5 and grid[cy, cx + 2] == 0.5):
-            return
-        else:
-            li = [Directions.UP.value, Directions.DOWN.value,
-                  Directions.LEFT.value, Directions.RIGHT.value]
-
-            while len(li) > 0:
-                dir = random.choice(li)
-                li.remove(dir)
-
-                if dir == Directions.UP.value:
-                    ny = cy - 2
-                    my = cy - 1
-                    nx = cx
-                    mx = cx
-                elif dir == Directions.DOWN.value:
-                    ny = cy + 2
-                    my = cy + 1
-                    nx = cx
-                    mx = cx
-                elif dir == Directions.LEFT.value:
-                    nx = cx - 2
-                    mx = cx - 1
-                    ny = cy
-                    my = cy
-                elif dir == Directions.RIGHT.value:
-                    nx = cx + 2
-                    mx = cx + 1
-                    ny = cy
-                    my = cy
-
-
-                if 0 < ny < self.height - 1 and 0 < nx < self.width - 1:
-
-                    if grid[ny, nx] != 0.5:
-
-                        grid[my, mx] = 0.5
-
-                        self.generator(nx, ny, grid)
+            if 0 < nx < self.width - 1 and 0 < ny < self.height - 1:
+                if grid[ny, nx] != 0.5:
+                    grid[my, mx] = 0.5
+                    self.generator(nx, ny, grid)
 
 if __name__ == "__main__":
     mazeGen = Backtracking(50, 50, "maze.png", True)
